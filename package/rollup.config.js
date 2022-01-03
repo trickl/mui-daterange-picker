95cbf7eb86a2ebf60c7e612c4302639e81f960a0
Cédric RICARD <ricard33@gmail.com>
2022-01-03 10:52:38 +0100

    Upgrade packages and reduce bundle size

--- package/rollup.config.js
+++ package/rollup.config.js
@@ -1,33 +1,14 @@
 import typescript from 'rollup-plugin-typescript2';
-import babel from 'rollup-plugin-babel';
-import commonjs from 'rollup-plugin-commonjs';
+import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
 import external from 'rollup-plugin-peer-deps-external';
-import resolve from 'rollup-plugin-node-resolve';
-import url from 'rollup-plugin-url';
+import resolve from '@rollup/plugin-node-resolve';
+import url from '@rollup/plugin-url';
 
 import pkg from './package.json';
 
 const commonjsOptions = {
   include: 'node_modules/**',
-  // left-hand side can be an absolute path, a path
-  // relative to the current directory, or the name
-  // of a module in node_modules
-  namedExports: {
-    'node_modules/react/index.js': [
-      'cloneElement',
-      'createContext',
-      'Component',
-      'createElement',
-    ],
-    'node_modules/react-dom/index.js': ['render', 'hydrate'],
-    'node_modules/react-is/index.js': [
-      'isElement',
-      'isFragment',
-      'isValidElementType',
-      'ForwardRef',
-      'Memo',
-    ],
-  },
 };
 
 export default {
@@ -44,11 +25,14 @@ export default {
       sourcemap: true,
     },
   ],
+  external: [/@babel\/runtime/],
   plugins: [
     external(),
     url({ exclude: ['**/*.svg'] }),
     babel({
+      babelHelpers: 'runtime',
       exclude: 'node_modules/**',
+      plugins: ["@babel/plugin-transform-runtime"],
     }),
     resolve(),
     typescript({
